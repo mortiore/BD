b315339e446945a420deed624c082448211698cf
josereis <josereis@unipam.edu.br>
2021-10-07 20:14:20 -0300

    Aula 2021-10-05

---
+++ Models/Table/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace eCommerce.Models.Table
+{
+    public partial class Pessoa
+    {
+        public Pessoa()
+        {
+            Enderecos = new HashSet<Endereco>();
+            Movimentos = new HashSet<Movimento>();
+            Telefones = new HashSet<Telefone>();
+        }
+
+        public int IdPessoa { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+        public long CpfCnpj { get; set; }
+        public string Email { get; set; }
+
+        public virtual Cliente Cliente { get; set; }
+        public virtual Fornecedor Fornecedor { get; set; }
+        public virtual Funcionario Funcionario { get; set; }
+        public virtual Loja Loja { get; set; }
+        public virtual PessoaFisica PessoaFisica { get; set; }
+        public virtual PessoaJuridica PessoaJuridica { get; set; }
+        public virtual ICollection<Endereco> Enderecos { get; set; }
+        public virtual ICollection<Movimento> Movimentos { get; set; }
+        public virtual ICollection<Telefone> Telefones { get; set; }
+    }
+}
